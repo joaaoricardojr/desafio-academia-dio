100cb521e8fb6c4947eca8cb627ece62528dcf9c
joaaoricardojr <joaoricardo.rsj@gmail.com>
2022-06-12 21:54:10 -0300

    inclusão projeto

---
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -0,0 +1,49 @@
+package me.dio.academia.digital.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_avaliacoes")
+public class AvaliacaoFisica {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "aluno_id")
+  private Aluno aluno;
+
+  private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+
+  @Column(name="peso_atual")
+  private double peso;
+
+  @Column(name="altura_atual")
+  private double altura;
+
+  public void setAluno(Aluno aluno2) {
+  }
+
+  public LocalDateTime getDataDaAvaliacao() {
+    return dataDaAvaliacao;
+  }
+
+  public void setDataDaAvaliacao(LocalDateTime dataDaAvaliacao) {
+    this.dataDaAvaliacao = dataDaAvaliacao;
+  }
+
+  public void setAluno(Object peso2) {
+  }
+
+  public void setAltura(Object altura2) {
+  }
+
+}
